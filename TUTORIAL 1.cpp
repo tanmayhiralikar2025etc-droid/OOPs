71a13767823d3660b6b9a169652d36815ad61885
tanmayhiralikar2025etc-droid <tanmayhiralikar2025.etc@mmcoe.edu.in>
2026-09-08 12:51:35 +0530

    Update TUTORIAL 1.cpp

--- TUTORIAL 1.cpp
+++ TUTORIAL 1.cpp
@@ -1,34 +1,19 @@
-//Write a  c++ program to print pascal triangle as per no of rows entered by the user.
 #include <iostream>
 using namespace std;
-
-int fact(int k){
-    int res = 1 ;
-    for (int i = k ; i>1 ; i--){
-        res*=i;
-    }
-    return res ; 
+int main() {
+int r;
+cout << "Enter the number of rows: ";
+cin >> r;
+for (int i = 0; i < r; i++) {
+for (int space = 1; space <= r - i; space++) {
+cout << " ";
 }
-
-int binoCoeff(int d , int m){
-    int coeff ; 
-    coeff = fact(d)/(fact(m)*fact(d-m));
-    return coeff ; 
+int n = 1;
+for (int j = 0; j <= i; j++) {
+cout << n << " ";
+n = n * (i - j) / (j + 1);
 }
-
-void printPascal(int s){
-    for(int i =0 ; i<s ; i++){
-        for(int j = 0 ; j<= i ; j++){
-            cout<<"\t"<<binoCoeff(i,j);
-        }
-        cout<<endl;
-    }
+cout<<endl;
 }
-
-int main(){
-    int r ; 
-    cout<<"Enter number of rows :";
-    cin>>r;
-    printPascal(r);
-    return 0;
+return 0;
 }
